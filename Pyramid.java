8b3d8f3753f6e7b813d4a1a3e85a20958965e332
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2022-02-14 10:30:01 +0000

    Update Pyramid.java
    Further refactoring

--- Pyramid.java
+++ Pyramid.java
@@ -1,38 +1,40 @@
 public class Pyramid {
 		
 	public static void main(String args[]) {
-		String pyramid = getPyramid(997, 9);
+		String pyramid = getPyramid(997, 10);
 		System.out.println(pyramid);
 	}
 
 	private static String getPyramid(int topOfPyramidNum, int numLevels) {
 		int lenTerminatingNumber = String.valueOf(topOfPyramidNum + numLevels).length();
 		int lenTerminatingLine = getFinalLineLength(topOfPyramidNum, numLevels);
-		int spacesBeforeNumOnRight = (lenTerminatingLine / 2) - (lenTerminatingNumber / 2);
-		int spacesBeforeNumOnLeft = 0;
+		int spacesBeforeFirstNumOnRow = (lenTerminatingLine / 2) - (lenTerminatingNumber / 2);
+		int spacesBeforeSecondNumOnRow = 0;
 		int currentNum = topOfPyramidNum;
 		int prevNum = 0;
 		String pyramid = "";
 		for(int rowNum = 0; rowNum < numLevels; rowNum++) {
-			pyramid += getIndentation(spacesBeforeNumOnRight);
+			pyramid += getEmptySpaces(spacesBeforeFirstNumOnRow);
 			pyramid += currentNum;
 			if(rowNum == 1) {
-				spacesBeforeNumOnLeft = String.valueOf(prevNum).length();
+				spacesBeforeSecondNumOnRow = String.valueOf(prevNum).length();
 			}
 			if(rowNum > 1) {
-				spacesBeforeNumOnLeft += (String.valueOf(prevNum).length() * 2);
+				spacesBeforeSecondNumOnRow += (String.valueOf(prevNum).length() * 2);
 			}
-			pyramid += rowNum > 0 ? getIndentation(spacesBeforeNumOnLeft) : ""; 
-			pyramid += rowNum > 0 ? currentNum : "";
+			// If after starting row, add spaces before the second number
+			if(rowNum > 0) {
+				pyramid += getEmptySpaces(spacesBeforeSecondNumOnRow) + currentNum;
+			}
 			pyramid += "\n";
 			prevNum = currentNum;
 			currentNum++;
-			spacesBeforeNumOnRight -= String.valueOf(currentNum).length(); 
+			spacesBeforeFirstNumOnRow -= String.valueOf(currentNum).length(); 
 		}
 		return pyramid;
 	}
 
-	private static String getIndentation(int indentationThisLine) {
+	private static String getEmptySpaces(int indentationThisLine) {
 		String emptySpaces = "";
 		for(int i = 0; i < indentationThisLine; i++) {
 			emptySpaces += " ";
@@ -44,8 +46,8 @@ public class Pyramid {
 		int numSpaces = 0;
 		for(int rowNumber = 0; rowNumber < numLevels; rowNumber++) {
 			int spacesOccupiedByNumber = String.valueOf(topOfPyramidNum).length();
-			numSpaces += spacesOccupiedByNumber; // The left occurrence of the number
-			numSpaces += rowNumber > 0 ? spacesOccupiedByNumber : 0; // The right occurrence of the number
+			numSpaces += spacesOccupiedByNumber; // The first occurrence of the number on the row
+			numSpaces += rowNumber > 0 ? spacesOccupiedByNumber : 0; // The second occurrence of the number on the row
 			topOfPyramidNum++;
 		}
 		return numSpaces % 2 == 0 ? numSpaces : numSpaces + 1;
